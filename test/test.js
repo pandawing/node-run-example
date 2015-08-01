8fcefda34285d07f403029c5937b661ef43c2453
sanemat <o.gata.ken@gmail.com>
2015-08-01 10:56:44 +0900

    chore(test): fix lint and test

--- test/test.js
+++ test/test.js
@@ -3,8 +3,5 @@ var assert = require('power-assert');
 var runExample = require('../');
 
 it('should ', function () {
-  assert.strictEqual(runExample.runExample('unicorns'), 'unicorns & rainbows');
-});
-it('should not ', function () {
-  assert.strictEqual(runExample.runExample('unicorns'), 'unicorns & wrong');
+  assert.equal(runExample.runExample('unicorns'), undefined);
 });
